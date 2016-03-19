806fcd3fb045184347d042c28bb76b64ad8ab87d
sea413 <seanmp42@gmail.com>
2016-03-18 20:05:17 -0700

    test for API parameters.

--- js/search.js
+++ js/search.js
@@ -1,8 +1,10 @@
 exports.getRepos = function(){
   var apiKey = "1acf7e584b9e702f76c2943985917b8042b78f66";
   var users = $('#users').val();
-  $.get('https://api.github.com/users/' + users + '?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + users + '/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
+    // console.log(response.id.description);
+    // console.log(response.id.clone_url);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
