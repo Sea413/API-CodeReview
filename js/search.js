df68cbae02043d7f4c7debfcc26d920e193faeae
sea413 <seanmp42@gmail.com>
2016-03-19 02:22:42 -0700

    add the readme file.

--- js/search.js
+++ js/search.js
@@ -1,10 +1,9 @@
 exports.getRepos = function(){
-  var apiKey = "1acf7e584b9e702f76c2943985917b8042b78f66";
+  var apiKey = require('./../.env').apiKey;
   var users = $('#users').val();
   $.get('https://api.github.com/users/' + users + '/repos?access_token=' + apiKey).then(function(response){
-    console.log(response);
-    for(var i = 0; i < response.Length; i++) {
-
+    console.log(response.length);
+    for(var i = 0; i < response.length; i++) {
     $('#api-results').append("<li>" + response[i].name + "</li>");
     $('#api-results').append("<li>" + response[i].description + "</li>");
   }
